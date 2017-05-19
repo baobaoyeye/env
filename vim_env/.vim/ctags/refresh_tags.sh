1cb57a3735bef033c95c083ffc201899023ae595
baobaoyeye <bao.renyi@gmail.com>
2017-05-19 14:36:53 +0800

    to tar.gz

--- vim_env/.vim/ctags/refresh_tags.sh
+++
@@ -1,3 +0,0 @@
-ctags -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+ialS --extra=+q -R  -f systags /opt/compiler/gcc-4.8.2/include/
-
-ctags -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+ialS --extra=+q -R  -f wstags /home/work/workspace/
